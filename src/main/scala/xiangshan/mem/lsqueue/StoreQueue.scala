962e21704f45332d0f0d3f93feb14ab52e0274d3
Anzooooo <anzoso@outlook.com>
2025-05-20 17:46:55 +0800

    fix(StoreQueue): redirect logic for vector exception

--- src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
+++ src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
@@ -1459,7 +1459,7 @@ class StoreQueue(implicit p: Parameters) extends XSModule
   // misprediction recovery / exception redirect
   // invalidate sq term using robIdx
   for (i <- 0 until StoreQueueSize) {
-    needCancel(i) := (uop(i).robIdx.needFlush(io.brqRedirect) & !isVec(i) || isAfter(uop(i).robIdx, io.brqRedirect.bits.robIdx) && io.brqRedirect.valid && isVec(i)) && allocated(i) && !committed(i)
+    needCancel(i) := (uop(i).robIdx.needFlush(io.brqRedirect) && !vecExceptionFlag.valid || isAfter(uop(i).robIdx, io.brqRedirect.bits.robIdx) && io.brqRedirect.valid && vecExceptionFlag.valid) && allocated(i) && !committed(i)
     when (needCancel(i)) {
       allocated(i) := false.B
       completed(i) := false.B
